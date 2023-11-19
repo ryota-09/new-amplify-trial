b2fb17c15b8ed6e139295dc56373b4ecfc85e890
ryota-09 <n.ryo950911@gmail.com>
2023-11-19 13:15:10 +0900

    14用のライブラリを削除

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,5 +1,4 @@
 import type { NextRequest } from 'next/server';
-import { ImageResponse } from 'next/og';
 import type { ReactElement } from 'react';
 
 export const runtime = 'edge';
@@ -8,8 +7,8 @@ const interSemiBold = fetch(
   new URL('./Inter-SemiBold.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
-export async function GET(req: NextRequest): Promise<Response | ImageResponse> {
-  console.log("@@@@@@@@@@@@@@@@@@@","api/og/route.tsx call")
+export async function GET(req: NextRequest): Promise<Response> {
+  console.log('@@@@@@@@@@@@@@@@@@@', 'api/og/route.tsx call');
   try {
     const { searchParams } = new URL(req.url);
     const isLight = req.headers.get('Sec-CH-Prefers-Color-Scheme') === 'light';
@@ -18,55 +17,9 @@ export async function GET(req: NextRequest): Promise<Response | ImageResponse> {
       ? searchParams.get('title')
       : 'App Router Playground';
 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            position: 'relative',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          {isLight ? <LightSvg /> : <DarkSvg />}
-          <div
-            style={{
-              position: 'absolute',
-              fontFamily: 'Inter',
-              fontSize: '48px',
-              fontWeight: '600',
-              letterSpacing: '-0.04em',
-              color: isLight ? 'black' : 'white',
-              top: '250px',
-              left: '50%',
-              transform: 'translateX(-50%)',
-              whiteSpace: 'pre-wrap',
-              maxWidth: '750px',
-              textAlign: 'center',
-              wordWrap: 'break-word',
-              overflowWrap: 'break-word',
-            }}
-          >
-            {title}
-          </div>
-        </div>
-      ),
-      {
-        width: 843,
-        height: 441,
-        fonts: [
-          {
-            name: 'Inter',
-            data: await interSemiBold,
-            style: 'normal',
-            weight: 400,
-          },
-        ],
-      },
-    );
+    return new Response(`image response`, { status: 200 });
   } catch (e) {
     if (!(e instanceof Error)) throw e;
-
     // eslint-disable-next-line no-console
     console.log(e.message);
     return new Response(`Failed to generate the image`, {
